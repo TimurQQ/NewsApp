b69b2e89132b3710a79b5ba78fa9bd0e7b790819
Tanya <tatyana.kazey1@gmail.com>
2021-05-25 01:15:19 +0300

    Create network+json data

---
+++ app/src/main/java/android_courses/newsapp/Constants.kt
@@ -0,0 +1,9 @@
+package android_courses.newsapp
+
+class Constants {
+    companion object {
+        const val API_KEY = "3d9ab1350f8147638ef11836a980cd56"
+        const val BASE_URL = "https://newsapi.org"
+
+    }
+}
---
+++ app/src/main/java/android_courses/newsapp/network/NewsAPI.kt
@@ -0,0 +1,17 @@
+package android_courses.newsapp.network
+
+import android_courses.newsapp.Constants.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    @GET("v2/top-headlines")
+    suspend fun getNews(
+        @Query("country")
+        countryCode: String = "ru",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    )
+}
