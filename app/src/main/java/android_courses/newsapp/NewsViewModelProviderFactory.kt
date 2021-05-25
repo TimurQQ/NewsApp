b1327cc0edda7e5c5dfd22c6bb9f99ff76da8f4b
Tanya <tatyana.kazey1@gmail.com>
2021-05-25 21:57:51 +0300

    Create NewsViewModelProviderFactory

---
+++ app/src/main/java/android_courses/newsapp/NewsViewModelProviderFactory.kt
@@ -0,0 +1,11 @@
+package android_courses.newsapp
+
+import android_courses.newsapp.repository.NewsRepository
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class NewsViewModelProviderFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return NewsViewModel(newsRepository) as T
+    }
+}
