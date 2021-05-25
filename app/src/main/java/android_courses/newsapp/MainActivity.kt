c5386aa1393ff7109824818255a948d58b9fff31
Tanya <tatyana.kazey1@gmail.com>
2021-05-25 22:03:01 +0300

    Link activity with NewsViewModel

--- app/src/main/java/android_courses/newsapp/MainActivity.kt
+++ app/src/main/java/android_courses/newsapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package android_courses.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android_courses.newsapp.model.NewsResponse
+import android_courses.newsapp.repository.NewsRepository
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsRepository = NewsRepository()
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel =ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+
     }
 }
--- app/src/main/java/android_courses/newsapp/fragments/NewsFragment.kt
+++ app/src/main/java/android_courses/newsapp/fragments/NewsFragment.kt
@@ -1,7 +1,26 @@
 package android_courses.newsapp.fragments
 
+import android.os.Bundle
+import android.view.View
+import android_courses.newsapp.MainActivity
+import android_courses.newsapp.NewsViewModel
 import androidx.fragment.app.Fragment
 import android_courses.newsapp.R
+import android_courses.newsapp.adapter.NewsAdapter
 
 class NewsFragment : Fragment(R.layout.fragment_news) {
+
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+
+
+
+
+
+
+    }
 }
