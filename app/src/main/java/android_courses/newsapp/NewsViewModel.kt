bd6a7f706e06c59740752002e008c24faedb7a4a
Tanya <tatyana.kazey1@gmail.com>
2021-05-25 21:54:35 +0300

    Create NewsViewModel+Resource

---
+++ app/src/main/java/android_courses/newsapp/NewsViewModel.kt
@@ -0,0 +1,29 @@
+package android_courses.newsapp
+
+import android_courses.newsapp.model.NewsResponse
+import android_courses.newsapp.repository.NewsRepository
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+}
--- app/src/main/java/android_courses/newsapp/network/NewsAPI.kt
+++ app/src/main/java/android_courses/newsapp/network/NewsAPI.kt
@@ -1,17 +1,19 @@
 package android_courses.newsapp.network
 
 import android_courses.newsapp.Constants.Companion.API_KEY
+import android_courses.newsapp.model.NewsResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsAPI {
     @GET("v2/top-headlines")
     suspend fun getNews(
-        @Query("country")
-        countryCode: String = "ru",
-        @Query("page")
-        pageNumber: Int = 1,
-        @Query("apiKey")
-        apiKey: String = API_KEY
-    )
+            @Query("country")
+            countryCode: String = "ru",
+            @Query("page")
+            pageNumber: Int = 1,
+            @Query("apiKey")
+            apiKey: String = API_KEY
+    ): Response<NewsResponse>
 }
