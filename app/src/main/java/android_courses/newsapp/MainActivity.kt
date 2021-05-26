2278f16b44fdc442da12c2789c736c8390b37a83
Tanya <tatyana.kazey1@gmail.com>
2021-05-26 19:12:17 +0300

    Everything works. Create the design.

--- app/src/main/java/android_courses/newsapp/MainActivity.kt
+++ app/src/main/java/android_courses/newsapp/MainActivity.kt
@@ -1,22 +1,24 @@
 package android_courses.newsapp
 
 import android.os.Bundle
+import android_courses.newsapp.fragments.NewsFragment
 import android_courses.newsapp.repository.NewsRepository
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val newsRepository = NewsRepository()
-        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
-        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
-
-
+        val newsFragment  = NewsFragment ()
+        makeCurrentFragment(newsFragment)
     }
+    private fun makeCurrentFragment(fragment: Fragment) =
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper, fragment)
+                commit()
+            }
 }
--- app/src/main/java/android_courses/newsapp/fragments/NewsFragment.kt
+++ app/src/main/java/android_courses/newsapp/fragments/NewsFragment.kt
@@ -3,13 +3,15 @@ package android_courses.newsapp.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android_courses.newsapp.MainActivity
 import android_courses.newsapp.NewsViewModel
+import android_courses.newsapp.NewsViewModelProviderFactory
 import android_courses.newsapp.R
 import android_courses.newsapp.Resource
 import android_courses.newsapp.adapter.NewsAdapter
+import android_courses.newsapp.repository.NewsRepository
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_news.*
 
@@ -20,9 +22,11 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewModel = (activity as MainActivity).viewModel
         setupRecyclerView()
+
+        val newsRepository = NewsRepository()
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
         viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
             when (response) {
