b6e37534605d6d93c1b237158d1ae1809f22cdb1
Kazak Kirill <kazzak.kirill1995@gmail.com>
2021-06-11 02:25:29 +0300

    CustomEditTextView v0.1 (все хорошо, только не знаю что делать с темным фоном)

---
+++ app/src/main/java/android_courses/newsapp/custom/CustomEditTextView.kt
@@ -0,0 +1,30 @@
+package android_courses.newsapp.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.EditText
+import android_courses.newsapp.R
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.custom_edit_text_view.view.*
+
+class CustomEditTextView @JvmOverloads constructor(
+        context: Context,
+        attrs : AttributeSet? = null,
+        defStyle : Int = 0
+):ConstraintLayout(context, attrs, defStyle) {
+
+    var hint : String = ""
+    set(value) {
+        field = value
+        findViewById<EditText>(R.id.custom_edit_text_view).hint = value
+    }
+
+    init {
+        View.inflate(context, R.layout.custom_edit_text_view, this)
+
+        with (context.theme.obtainStyledAttributes(attrs, R.styleable.CustomEditTextView, 0, 0)) {
+            hint = getString(R.styleable.CustomEditTextView_editHint) ?: ""
+        }
+    }
+}
