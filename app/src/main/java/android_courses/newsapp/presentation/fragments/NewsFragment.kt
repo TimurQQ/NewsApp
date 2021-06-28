709986a992637712a413057353acbdee4bda0242
Kazak Kirill <kazzak.kirill1995@gmail.com>
2021-06-28 17:22:29 +0300

    final debug

--- app/src/main/java/android_courses/newsapp/presentation/fragments/NewsFragment.kt
+++ app/src/main/java/android_courses/newsapp/presentation/fragments/NewsFragment.kt
@@ -69,9 +69,7 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
         if (keyWord == null) {
             viewModel.getBreakingNews("us")
         } else {
-            if (keyWord != null) {
                 viewModel.getNewsByKeyWord(keyWord)
-            }
         }
     }
 
